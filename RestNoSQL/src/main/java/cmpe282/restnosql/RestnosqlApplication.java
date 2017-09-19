a0719299ecdd6b8930dc0d5e76bec0f6049f6a85
kanghuawu <kang-hua.wu@sjsu.edu>
2017-09-18 22:45:17 -0700

    hw1 initial commit

---
+++ RestNoSQL/src/main/java/cmpe282/restnosql/RestnosqlApplication.java
@@ -0,0 +1,12 @@
+package cmpe282.restnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestnosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestnosqlApplication.class, args);
+	}
+}
---
+++ RestNoSQL/src/test/java/cmpe282/restnosql/RestnosqlApplicationTests.java
@@ -0,0 +1,16 @@
+package cmpe282.restnosql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestnosqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
